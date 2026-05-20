7be18587ef828bb44d3b1de9e6768ad6f76a4dfc
J-Babik <janbabik1@gmail.com>
2026-05-20 09:22:38 +0200

    feat: gui

--- main.py
+++ main.py
@@ -1,33 +1,104 @@
-from mapbook_lib.model import users
-from mapbook_lib.controler import read_users, add_user, delete_user, update_user, update_user_post, get_user_map
+from tkinter import *
 
-def main():
 
-    while True:
-        print("==========MENU============")
-        print("0 - zakończ program")
-        print("1 - wyświetl znajomych")
-        print("2 - dodaj znajomego")
-        print("3 - usuń znajomego")
-        print("4 - update znajomego")
-        print("5 - update posta")
-        print("6 - pokaż mapę")
-        choice = input("Wybierz opcję menu: ")
-        print(f"Wybrano opcję {choice}")
-        if choice == "0":
-            break
-        if choice == "1":
-            read_users(users)
-        if choice == "2":
-            add_user(users)
-        if choice == "3":
-            delete_user(users)
-        if choice == "4":
-            update_user(users)
-        if choice == "5":
-            update_user_post(users)
-        if choice == "6":
-            get_user_map(users)
+root = Tk()
 
-if __name__ == "__main__":
-    main()
+root.title("Mapbook_JB")
+root.geometry("1024x760")
+
+#FRAME
+
+ramka_lista_obiektow = Frame(root)
+ramka_formularz = Frame(root)
+ramka_szczegoly_obiektu = Frame(root)
+
+ramka_lista_obiektow.grid(row=0, column=0)
+ramka_formularz.grid(row=0, column=1)
+ramka_szczegoly_obiektu.grid(row=1, column=0, columnspan=2)
+
+#RAMKA LISTA OBIEKTOW
+
+label_lista_obiektow = Label(ramka_lista_obiektow, text="Lista znajomych: ")
+listbox_lista_obiektow = Listbox(ramka_lista_obiektow)
+
+button_pokaz_szczegoly_obiektu = Button(ramka_lista_obiektow, text="Pokaż szczegóły")
+button_edytuj_obiekt = Button(ramka_lista_obiektow, text="Edytuj")
+button_usun_obiekt = Button(ramka_lista_obiektow, text="Usuń")
+
+label_lista_obiektow.grid(row=0, column=0)
+listbox_lista_obiektow.grid(row=1, column=0)
+button_pokaz_szczegoly_obiektu.grid(row=2, column=0)
+button_edytuj_obiekt.grid(row=2, column=1)
+button_usun_obiekt.grid(row=2, column=2)
+
+
+#RAMKA FORMULARZ
+
+label_formularz = Label(ramka_formularz, text="Formularz: ")
+label_imie = Label(ramka_formularz, text="Imię: ")
+label_nazwisko = Label(ramka_formularz, text="Nazwisko: ")
+label_liczba_postow = Label(ramka_formularz, text="Liczba postów: ")
+label_lokalizacja = Label(ramka_formularz, text="Lokalizacja: ")
+
+
+label_formularz.grid(row=0, column=0)
+label_imie.grid(row=1, column=0)
+label_nazwisko.grid(row=2, column=0)
+label_liczba_postow.grid(row=3, column=0)
+label_lokalizacja.grid(row=4, column=0)
+
+entry_imie = Entry(ramka_formularz)
+entry_nazwisko = Entry(ramka_formularz)
+entry_liczba_postow = Entry(ramka_formularz)
+entry_lokalizacja = Entry(ramka_formularz)
+
+entry_imie.grid(row=1, column=1)
+entry_nazwisko.grid(row=2, column=1)
+entry_liczba_postow.grid(row=3, column=1)
+entry_lokalizacja.grid(row=4, column=1)
+
+button_dodaj_uzytkownika = Button(ramka_formularz, text="Dodaj znajomego")
+
+button_dodaj_uzytkownika.grid(row=5, column=0, columnspan=2)
+
+
+#SZCZEGÓŁY OBIEKTU
+
+label_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Szczegóły użytkownika: ")
+
+
+label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Imię: ")
+label_imie_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
+label_nazwisko_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Nazwisko: ")
+label_nazwisko_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
+label_liczba_postow_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Liczba postów: ")
+label_liczba_postow_wartosc = Label(ramka_szczegoly_obiektu, text="...")
+label_lokalizacja_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text="Lokalizacja: ")
+label_lokalizacja_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
+
+
+label_szczegoly_obiektu.grid(row=0, column=0)
+label_imie_szczegoly_obiektu.grid(row=1, column=0)
+label_imie_szczegoly_obiektu_wartosc.grid(row=1, column=1)
+label_nazwisko_szczegoly_obiektu.grid(row=1, column=3)
+label_nazwisko_szczegoly_obiektu_wartosc.grid(row=1, column=4)
+label_liczba_postow_szczegoly_obiektu.grid(row=1, column=5)
+label_liczba_postow_wartosc.grid(row=1, column=6)
+label_lokalizacja_szczegoly_obiektu.grid(row=1, column=7)
+label_lokalizacja_szczegoly_obiektu_wartosc.grid(row=1, column=8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
